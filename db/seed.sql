11d63f288e5688d6c1e942792b2fd45f9fb5e4a2
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-13 11:30:59 +0400

    Add seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+-- Sample seed data
+INSERT INTO categories (name) VALUES ('Electronics'), ('Apparel');
+INSERT INTO products (name, price, category_id) VALUES ('Phone', 699.00, 1), ('T-Shirt', 19.99, 2);
